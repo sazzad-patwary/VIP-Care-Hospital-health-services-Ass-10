360a0974a50406fbab1d91340ec90337edc75ca1
sazzad-patwary <sazzadpatwarysp@gmail.com>
2021-10-20 12:20:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Services from './Pages/Home/Services/Services';
+import Specialists from './Pages/Home/Specialists/Specialists';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Register from './Pages/Register/Register';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="/service">
+            <Services></Services>
+          </Route>
+          <Route path="/specialist">
+            <Specialists></Specialists>
+          </Route>
+          <Route path="">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Specialists from '../Specialists/Specialists';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Specialists></Specialists>
+        </div>
+    );
+};
+
+export default Home;
